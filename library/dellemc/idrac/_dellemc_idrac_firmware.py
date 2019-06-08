2d46441f0a276c6a3f267939857c3d6d44c924b1
rajeevarakkal <36444805+rajeevarakkal@users.noreply.github.com>
2019-06-08 01:36:01 -0700

    Removed the deprected module previous copy

--- library/dellemc/idrac/_dellemc_idrac_firmware.py
+++ library/dellemc/idrac/_dellemc_idrac_firmware.py
@@ -4,11 +4,9 @@
 #
 # Dell EMC OpenManage Ansible Modules
 # Version 2.0
-# Copyright (C) 2019 Dell Inc.
+# Copyright (C) 2018-2019 Dell Inc. or its subsidiaries. All Rights Reserved.
 
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
-# All rights reserved. Dell, EMC, and other trademarks are trademarks of Dell Inc. or its subsidiaries.
-# Other trademarks may be trademarks of their respective owners.
 
 
 from __future__ import (absolute_import, division, print_function)
